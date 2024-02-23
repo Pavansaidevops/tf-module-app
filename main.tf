f92bd94d7b11f5d2f012812a0664892c8d681357
Pavan Sai <pavansaidevops7@gmail.com>
2024-02-23 17:14:04 +0530

    terraform alb

--- main.tf
+++ main.tf
@@ -76,3 +76,19 @@ resource "aws_lb_target_group" "main" {
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
 }
+
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = var.listener
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["${var.component}-${var.env}.pavansai.online"]
+    }
+  }
+}
