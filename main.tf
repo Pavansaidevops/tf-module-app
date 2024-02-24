d8d9159fc5745453e102427ab25db41c0489daed
Pavan Sai <pavansaidevops7@gmail.com>
2024-02-24 15:43:41 +0530

    terraform app

--- main.tf
+++ main.tf
@@ -65,10 +65,10 @@ resource "aws_autoscaling_group" "main" {
 
 resource "aws_route53_record" "main" {
   zone_id = var.zone_id
-  name    = "${var.component}-${var.env}"
+  name    = var.component == "frontend" ? var.env : "${var.component}-${var.env}"
   type    = "CNAME"
   ttl     = 30
-  records = [var.alb_name]
+  records = [var.component == "frontend" ? var.public_alb_name : var.private_alb_name]
 }
 
 resource "aws_lb_target_group" "main" {
@@ -79,7 +79,7 @@ resource "aws_lb_target_group" "main" {
 }
 
 resource "aws_lb_listener_rule" "main" {
-  listener_arn = var.listener
+  listener_arn = var.private_listener
   priority     = var.lb_priority
 
   action {
@@ -89,7 +89,15 @@ resource "aws_lb_listener_rule" "main" {
 
   condition {
     path_pattern {
-      values = ["${var.component}-${var.env}.pavansai.online"]
+      values = [var.component == "frontend" ? "${var.env}.pavansai.online" : "${var.component}-${var.env}.pavansai.online"]
     }
   }
+}
+
+resource "aws_lb_target_group" "public" {
+  count        = var.component == "frontend" ? 1: 0
+  name         = "${local.name_prefix}-public"
+  port         = var.port
+  protocol     = "IP"
+  vpc_id       = var.vpc_id
 }
