5067cf05ce15272d4f09b6ef887eb94604fe2a83
Pavan Sai <pavansaidevops7@gmail.com>
2024-03-07 12:54:34 +0530

    terraform roboshop app

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_lb_listener_rule" "main" {
   }
 
   condition {
-    path_pattern {
+    host_header {
       values = [
         var.component == "frontend" ? "${var.env}.pavansai.online" : "${var.component}-${var.env}.pavansai.online"
       ]
@@ -97,10 +97,17 @@ resource "aws_lb_listener_rule" "main" {
 }
 
 resource "aws_lb_target_group" "public" {
-  count = var.component == "frontend" ? 1 : 0
+  count    = var.component == "frontend" ? 1 : 0
   name     = "${local.name_prefix}-public"
   port     = var.port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
 
+resource "aws_lb_target_group_attachment" "public" {
+  count             = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
+  target_group_arn  = aws_lb_target_group.public[0].arn
+  target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
+  port              = 80
+  availability_zone = "all"
+}
